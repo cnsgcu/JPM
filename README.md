e463412d7aaab5f10451f5d149acf5d0f102f371
Cuong Cu <csc823@gmail.com>
2015-02-24 00:48:33 -0600

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # JPM
+
+An attempt to emulate pattern matching commonly found in functional programmings using Java 8.
+
+# Purpose
+
+To practice Java 8
+
+# Tasks
+
+- [ ] Binary
+- [ ] Collection
+- [x] String
+- [x] Regex
+- [x] Boolean
+- [x] Integer
+- [x] Float
+- [x] Double
+- [x] Long
+- [x] Class
--- src/main/java/home/journal/Playground.java
+++ src/main/java/home/journal/Playground.java
@@ -1,8 +1,27 @@
 package home.journal;
 
+import home.journal.matcher.Matcher;
+
+import java.util.Optional;
+
+import static home.journal.matcher.Extractor.Of;
+
 public class Playground
 {
     public static void main(String[] args)
     {
+        final Object obj = "Match me!";
+
+        final Object rst = Matcher.match(obj)
+            .inCase(Of(1)              , Optional::get)
+            .inCase(Of(Integer.class)  , Optional::get)
+            .inCase(Of(true)           , Optional::get)
+            .inCase(Of(false)          , Optional::get)
+            .inCase(Of(Boolean.class)  , Optional::get)
+            .inCase(Of("match me not!"), Optional::get)
+            .inCase(Of(String.class)   , Optional::get)
+            .otherwise(() -> "WTF!");
+
+        System.out.println(rst);
     }
 }
